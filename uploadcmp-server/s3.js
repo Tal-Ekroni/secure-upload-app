21c074a4eda7d5bd8bd1b6593f0a87ff7ee704a0
tal <talekroni01@gmail.com>
2022-07-16 01:27:58 +0300

    keys deletion

--- uploadcmp-server/s3.js
+++ uploadcmp-server/s3.js
@@ -1,9 +1,5 @@
 import aws from 'aws-sdk'
 
-const region="us-east-1"
-const bucketName="upload-direct-s3-cmp"
-const accessKeyId="AKIAXO45LHCWABMSOPNM"
-const secretAccessKey="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"
 
 const s3= new aws.S3({
     region,
