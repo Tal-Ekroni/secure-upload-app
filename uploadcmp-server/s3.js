4f68f22f40d4d34786265cd9b2604a25c588e2d8
tal <talekroni01@gmail.com>
2022-07-16 21:00:47 +0300

    added env

--- uploadcmp-server/s3.js
+++ uploadcmp-server/s3.js
@@ -1,27 +1,34 @@
 import aws from 'aws-sdk'
 
-const region=""
-const bucketName=""
-const accessKeyId=""
-const secretAccessKey=""
+import dotenv from 'dotenv';
+dotenv.config();
 
-const s3= new aws.S3({
+const region = "us-east-1"
+const bucketName = "upload-direct-s3-cmp"
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+
+
+const s3 = new aws.S3({
     region,
     accessKeyId,
     secretAccessKey,
-    signatureVersion:'v4'
+    signatureVersion: 'v4'
 })
 
-export async function generateUploadURL(fileName,fileType){
-    const params=({
-        Bucket:bucketName,
-        Key:fileName,
-        Expires:120,
-        ContentType: fileType, 
-        
-    })
+export async function generateUploadURL(fileName, fileType) {
+    try {
+        const params = ({
+            Bucket: bucketName,
+            Key: fileName,
+            Expires: 120,
+            ContentType: fileType,
 
-const uploadURL= await s3.getSignedUrlPromise('putObject',params)
-
-return uploadURL
+        })
+        const uploadURL = await s3.getSignedUrlPromise('putObject', params)
+        return uploadURL
+    } catch (err) {
+        console.log('while generating url', err)
+        throw err
+    }
 }
--- uploadcmp-server/server.js
+++ uploadcmp-server/server.js
@@ -2,14 +2,20 @@ import express from 'express'
 import cors from 'cors'
 import { generateUploadURL } from './s3.js'
 
+
 var app = express()
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 const port = 3030
 
 app.get('/api/s3url', async (req, res) => {
-    const {fileName,fileType}=req.query
-    const url = await generateUploadURL(fileName,fileType)
-    res.send({ url })
+    try {
+        const { fileName, fileType } = req.query
+        const url = await generateUploadURL(fileName, fileType)
+        res.send({ url })
+    } catch (err) {
+        console.log('while generating url', err)
+        throw err
+    }
 })
 
 app.listen(port, () => {
